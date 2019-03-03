bbeb63ece40816bf196858272f6819c93fc340f1
David zhang <d.ydzdavid@yahoo.com>
2019-03-03 12:30:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -138,12 +138,24 @@ end
   
 
 
-def turn_count (board)
-  
+ef turn_count (board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+  counter += 1  
+    end
+  end
+  counter
 end
-
-def current_player(board)
-  
+    
+    
+    
+def current_player(board)  
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else 
+    return "O"
+  end
 end
 
 
